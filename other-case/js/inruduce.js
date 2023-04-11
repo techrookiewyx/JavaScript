31e3e40130cbcd76889289ad4ee84861f1735c20
wangyongxin <676425826@qq.com>
2023-04-11 17:15:13 +0800

    模块化

--- other-case/js/inruduce.js
+++ other-case/js/inruduce.js
@@ -1,1 +1,9 @@
-document.write("哈哈-外部引入");
+//document.write("哈哈-外部引入");
+
+//使用require（“path”）函数来引入模块，返回一个对象
+let req = require("./commonjs-require");
+console.log(req);
+
+//引入核心模块
+let path = require("path");
+console.log(path);
